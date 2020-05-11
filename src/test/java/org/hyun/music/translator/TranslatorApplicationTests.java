afb73bf03163e7393e3894f32acb539af149f63f
Andrew Hyun <andrew.hyun@alumni.stonybrook.edu>
2020-05-11 15:33:51 -0400

    init-commit

---
+++ src/test/java/org/hyun/music/translator/TranslatorApplicationTests.java
@@ -0,0 +1,13 @@
+package org.hyun.music.translator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TranslatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
