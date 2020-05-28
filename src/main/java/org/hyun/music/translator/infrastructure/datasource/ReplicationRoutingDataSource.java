964342a05f5f8b082ab6e2f339bee27320a904d3
Andrew Hyun <andrew.hyun@alumni.stonybrook.edu>
2020-05-27 22:35:27 -0400

    Setting up AbstractRoutingDataSource to allow using different configurations for read vs write transactions (For now it's same endpoint so it doesn't do anything different).

---
+++ src/main/java/org/hyun/music/translator/infrastructure/datasource/ReplicationRoutingDataSource.java
@@ -0,0 +1,12 @@
+package org.hyun.music.translator.infrastructure.datasource;
+
+import org.springframework.jdbc.datasource.lookup.AbstractRoutingDataSource;
+import org.springframework.transaction.support.TransactionSynchronizationManager;
+
+public class ReplicationRoutingDataSource extends AbstractRoutingDataSource {
+
+    @Override
+    protected Object determineCurrentLookupKey() {
+        return TransactionSynchronizationManager.isCurrentTransactionReadOnly() ? "read" : "write";
+    }
+}
---
+++ src/main/java/org/hyun/music/translator/infrastructure/datasource/config/ReplicationDataSourceConfig.java
@@ -0,0 +1,72 @@
+package org.hyun.music.translator.infrastructure.datasource.config;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import org.hyun.music.translator.infrastructure.datasource.ReplicationRoutingDataSource;
+import org.hyun.music.translator.infrastructure.datasource.config.properties.DatabaseConfigProperties;
+import org.hyun.music.translator.infrastructure.datasource.config.properties.DatabaseReadOnlyConfigProperties;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.DependsOn;
+import org.springframework.context.annotation.Primary;
+import org.springframework.jdbc.datasource.LazyConnectionDataSourceProxy;
+
+import javax.sql.DataSource;
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+public class ReplicationDataSourceConfig {
+    @Autowired
+    private DatabaseConfigProperties databaseConfigProperties;
+
+    @Autowired
+    private DatabaseReadOnlyConfigProperties databaseReadOnlyConfigProperties;
+
+    /**
+     * Main Datasource
+     * <p>Application must use this dataSource.</p>
+     */
+    @Primary
+    @Bean
+    // @DependsOn required so we can ensure these beans are initialized before initializing this bean!
+    // Note: readOnlyDataSource should be implemented if we plan to separate out read hits and write hits (for performance)
+    @DependsOn({"writeDataSource", "routingDataSource"})
+    public DataSource dataSource() { return new LazyConnectionDataSourceProxy(routingDataSource());}
+
+    @Bean
+    public DataSource routingDataSource(){
+        ReplicationRoutingDataSource routingDataSource = new ReplicationRoutingDataSource();
+
+        Map<Object, Object> dataSourceMap = new HashMap<>();
+        dataSourceMap.put("write", writeDataSource());
+        dataSourceMap.put("read", readDataSource());
+        routingDataSource.setTargetDataSources(dataSourceMap);
+        routingDataSource.setDefaultTargetDataSource(writeDataSource());
+
+        return routingDataSource;
+    }
+
+    @Bean(destroyMethod = "close")
+    public DataSource writeDataSource() {
+        HikariConfig config = new HikariConfig();
+        config.setDataSourceClassName(databaseConfigProperties.getDataSourceClassName());
+        config.addDataSourceProperty("url", databaseConfigProperties.getUrl());
+        config.addDataSourceProperty("user", databaseConfigProperties.getUsername());
+        config.addDataSourceProperty("password", databaseConfigProperties.getPassword());
+
+        return new HikariDataSource(config);
+    }
+
+    @Bean(destroyMethod = "close")
+    public DataSource readDataSource() {
+        HikariConfig config = new HikariConfig();
+        config.setDataSourceClassName(databaseReadOnlyConfigProperties.getDataSourceClassName());
+        config.addDataSourceProperty("url", databaseReadOnlyConfigProperties.getUrl());
+        config.addDataSourceProperty("user", databaseReadOnlyConfigProperties.getUsername());
+        config.addDataSourceProperty("password", databaseReadOnlyConfigProperties.getPassword());
+
+        return new HikariDataSource(config);
+    }
+}
